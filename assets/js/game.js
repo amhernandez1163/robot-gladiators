eb6498aa129c1cdb6c9f255802551f43558b7610
Amanda Dell <amhernandez1163@gmail.com>
2022-02-20 22:30:55 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -47,19 +47,25 @@ var fight = function(enemyName) {
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
-    
-        // check player's health
+           // check player's health
         if (playerHealth <= 0){
             window.alert(playerName + " has died!");
             break;
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
+        } 
     }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    } 
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
